0ef4b65b56e411330dbc4ef970155aa2cfbe3f6f
AaronNath11 <aaronsuhaendi@gmail.com>
2026-05-17 01:30:23 +0700

    feat: inisialisasi awal spring cloud gateway

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmart_api_gateway/BidmartApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidmartApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bidmart_api_gateway/BidmartApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmart_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
